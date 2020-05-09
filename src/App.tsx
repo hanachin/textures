5c863eb38419b2317012af6ae9aa8529896e31b3
Seiei Miyagi <hanachin@gmail.com>
2020-05-10 02:44:53 +0900

    Gingham, DiagnolGingham, Ichimatsu, Uroko

--- src/App.tsx
+++ src/App.tsx
@@ -21,6 +21,35 @@ const LeftDiagonalLine = styled.div`
   box-shadow: rgba(0, 0, 0, .15) 0 0 2px;
   background: repeating-linear-gradient(45deg, #fe0000 0 10px, transparent 0 20px);
 `;
+const Gingham = styled.div`
+  border-radius: 4px;
+  box-shadow: rgba(0, 0, 0, .15) 0 0 2px;
+  background: repeating-linear-gradient(#fe0000 0 2px, transparent 0 4px),
+              repeating-linear-gradient(90deg, #fe0000 0 2px, transparent 0 4px);
+`;
+const DiagonalGingham = styled.div`
+  border-radius: 4px;
+  box-shadow: rgba(0, 0, 0, .15) 0 0 2px;
+  background: repeating-linear-gradient(-45deg, #fe0000 0 2px, transparent 0 4px),
+              repeating-linear-gradient(45deg, #fe0000 0 2px, transparent 0 4px);
+`;
+const Ichimatsu = styled.div`
+  border-radius: 4px;
+  box-shadow: rgba(0, 0, 0, .15) 0 0 2px;
+  background: 0 0 / 8px 8px linear-gradient(45deg, #fe0000 25%, transparent 0),
+              4px 4px / 8px 8px linear-gradient(45deg, transparent 75%, #fe0000 0),
+              4px 4px / 8px 8px linear-gradient(45deg, #fe0000 25%, transparent 0),
+              8px 8px / 8px 8px linear-gradient(45deg, transparent 75%, #fe0000 0);
+`;
+const Uroko = styled.div`
+  border-radius: 4px;
+  box-shadow: rgba(0, 0, 0, .15) 0 0 2px;
+  background: 0 0 / 8px 16px linear-gradient(60deg, #fe0000 4px, transparent 0),
+              0 0 / 8px 16px linear-gradient(-60deg, #fe0000 4px, transparent 0),
+              4px 8px / 8px 16px linear-gradient(60deg, #fe0000 4px, transparent 0),
+              4px 8px / 8px 16px linear-gradient(-60deg, #fe0000 4px, transparent 0);
+              ;
+`;
 
 const AppRoot = styled.div`
   box-sizing: border-box;
@@ -40,6 +69,10 @@ const App = () => {
       <VerticalBorder/>
       <RightDiagonalLine/>
       <LeftDiagonalLine/>
+      <Gingham/>
+      <DiagonalGingham/>
+      <Ichimatsu/>
+      <Uroko/>
     </AppRoot>
   );
 }
