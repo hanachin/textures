19669f18578f58e7364f14b144fe9a6f102c57ba
Seiei Miyagi <hanachin@gmail.com>
2020-05-18 02:01:52 +0900

    Add FlowerBlock

--- src/App.tsx
+++ src/App.tsx
@@ -2870,6 +2870,51 @@ const Weed = styled(Example)`
               ),
               var(--secondary-color);
 `;
+const FlowerBlock = styled(Example)`
+  --circle-size: 24px;
+  --border-width: 8px;
+  --width: calc(3 * (var(--circle-size) + var(--border-width)));
+  --height: calc(var(--circle-size) * 2 + var(--border-width));
+  background: 0 0 / var(--width) var(--height)
+              linear-gradient(
+                var(--primary-color) 0 var(--border-width),
+                transparent 0
+              ),
+              calc(var(--border-width) / -2) 0 / var(--width) var(--height)
+              linear-gradient(
+                90deg,
+                var(--primary-color) 0 var(--border-width),
+                transparent 0
+              ),
+              0 calc(var(--circle-size) + var(--border-width)) / var(--width) var(--height)
+              radial-gradient(
+                circle at bottom center,
+                transparent 0,
+                transparent calc(var(--circle-size) - var(--border-width)),
+                var(--primary-color) 0 var(--circle-size),
+                transparent var(--circle-size),
+                transparent
+              ),
+              calc(var(--width) / 4) calc(var(--circle-size) * 2 + var(--border-width)) / var(--width) var(--height)
+              radial-gradient(
+                circle at bottom center,
+                transparent 0,
+                transparent calc(var(--circle-size) - var(--border-width)),
+                var(--primary-color) 0 var(--circle-size),
+                transparent var(--circle-size),
+                transparent
+              ),
+              calc(var(--width) / -4) calc(var(--circle-size) * 2 + var(--border-width)) / var(--width) var(--height)
+              radial-gradient(
+                circle at bottom center,
+                transparent 0,
+                transparent calc(var(--circle-size) - var(--border-width)),
+                var(--primary-color) 0 var(--circle-size),
+                transparent var(--circle-size),
+                transparent
+              ),
+              var(--secondary-color);
+`;
 
 const Header = styled.header`
   text-align: center;
@@ -2951,6 +2996,7 @@ const App = () => {
         <Flower/>
         <Prism/>
         <Weed/>
+        <FlowerBlock/>
       </Gallary>
     </AppRoot>
   );
